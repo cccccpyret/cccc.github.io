1f0454ec0232a16d87b7857ea8bcdcbea207bee8
cccccpyret <candice.cheung@tis.edu.mo>
2025-06-03 17:45:02 +0800

    Update largelocationbutton.swift
    jun 3

--- largelocationbutton.swift
+++ largelocationbutton.swift
@@ -1,51 +1,72 @@
 //
-//  largelocationbutton.swift
-//  travelitineray
+//  LargeLocationButton.swift
+//  Travel Itinerary
 //
-//  Created by Candice Cheung on 20/05/2025.
+//  Created by Candice Cheung on 27/05/2025.
 //
 
 import SwiftUI
 
 struct largelocationbutton: View {
+    
+    var city:String
+    var cost:Int
+    var averageRating:Double
+    var numberOfRatings:Int
+    var imageString:String
+    var height:CGFloat = 330
+    
     var body: some View {
-        VStack{
-            Image(systemName: "")
+        VStack(spacing:0){
+            
+            Image(imageString)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth:0, maxWidth: .infinity)
                 .frame(height:200)
+            
                 .background(Color.green)
+                .clipped()
             HStack {
                 VStack(alignment: .leading){
-                    Text("City")
+                    Text(city)
                         .font(.system(size: 30, weight: .bold))
-                    Text("Cost")
+                    Text("Starting at $\(cost)+")
                         .foregroundColor(.gray)
-                    
+                        .font(.system(size: 15))
                 }
                 Spacer()
                 VStack(alignment: .trailing){
                     HStack{
                         ForEach(0..<4){_ in
-                            Image(systemName: "star.fill").foregroundColor(Color.init(red: 220/255, green: 220/255, blue: 0))
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.init(red: 220/255, green: 220/255, blue: 0))
                         }
                     }.frame(width:150)
                     HStack{
-                        Text("1000+ ratings")
-//                          .frame(width: 70)
+                        Text("\(numberOfRatings)+ ratings")
+                            .frame(width: 70)
                             .foregroundColor(.gray)
                             .font(.system(size: 12))
-                        Text("3.5/5")
+                        Text("\(averageRating)/5")
                             .font(.system(size: 20))
                             .frame(width: 60)
                     }
                 }
-            }
-            }
-        .padding()
+            }.frame(height:70)
+            .padding()
+        }
+        
+        
         .background(Color.init(red: 0.9, green: 0.9, blue: 0.9))
         .cornerRadius(10)
-        }
+        //.shadow(radius:4, y:4)
+        .frame(height: height)
+        .clipped()
+        
     }
+}
 
 #Preview {
-    largelocationbutton()
+    largelocationbutton(city: "City", cost: 0, averageRating: 2.5, numberOfRatings: 1000, imageString: "")
 }
